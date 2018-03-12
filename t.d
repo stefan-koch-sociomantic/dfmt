ed1692762ba863e9d33e0091b8af1bbda2b52895
Stefan Koch <stefan.koch@sociomantic.com>
2018-03-12 15:59:11 +0100

    Temporarily add test-file

---
+++ t.d
@@ -0,0 +1,27 @@
+
+public static istring[][] lines = [
+	["field1", "field2", "field3"],
+	["1", "val1"],
+	["2", "val2"],
+	["3", "val3"],
+	["4", "val4"],
+	["5", "val5"],
+	["6", "val6"]
+];
+
+
+class C
+{
+const int x = 2;
+auto y = cast()x;
+}
+
+void main()
+{
+	float[3][3] mat = [
+		[234.3456, 42435.8653, 23.5],
+		[3.245, 235.3, 234.664],
+		[14324.6453, 23434.645, 9678.345]
+	];
+}
+
